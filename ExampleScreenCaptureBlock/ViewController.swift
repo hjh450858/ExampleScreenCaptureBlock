236ed3fb4b60d2818238a95e0502507d8bb493d3
hwang jae hyeon <hjh450858@gmail.com>
2023-03-13 14:11:08 +0900

    first git

--- ExampleScreenCaptureBlock/ViewController.swift
+++ ExampleScreenCaptureBlock/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var label: UILabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.addSubview(label)
+        
+        label.text = "캡처 불가능"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
-
-
 }
 
